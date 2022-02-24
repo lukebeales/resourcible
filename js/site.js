15717fc2871ca027b7928aa7c78e675a7edece9d
Luke Beales <lukebeales@gmail.com>
2022-02-24 06:25:16 +0000

    initial commit

---
+++ js/site.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+  // Variables
+  var $codeSnippets = $('.code-example-body'),
+      $body = $('body'),
+      $popoverLink = $('[data-popover]'),
+      $document = $(document);
+
+  function init() {
+    $popoverLink.on('click', openPopover)
+    $document.on('click', closePopover)
+  }
+
+  function openPopover(e) {
+    e.preventDefault()
+    closePopover();
+    var popover = $($(this).data('popover'));
+    popover.toggleClass('open')
+    e.stopImmediatePropagation();
+  }
+
+  function closePopover(e) {
+    if($('.popover.open').length > 0) {
+      $('.popover').removeClass('open')
+    }
+  }
+
+  init();
+
+});
