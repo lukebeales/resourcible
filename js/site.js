970134082dab9908d16677ea78932ef890bcca85
Luke Beales <lukebeales@gmail.com>
2022-03-07 01:08:43 +0000

    adding recaptcha enterprise in

--- js/site.js
+++ js/site.js
@@ -1,61 +1,61 @@
 $(document).ready(function() {
+	// Variables
+	var $codeSnippets = $('.code-example-body'),
+		$body = $('body'),
+		$popoverLink = $('[data-popover]'),
+		$document = $(document);
 
-  // Variables
-  var $codeSnippets = $('.code-example-body'),
-      $body = $('body'),
-      $popoverLink = $('[data-popover]'),
-      $document = $(document);
+	function init() {
+		$popoverLink.on('click', openPopover)
+		$document.on('click', closePopover)
+	}
 
-  function init() {
-    $popoverLink.on('click', openPopover)
-    $document.on('click', closePopover)
-  }
+	function openPopover(e) {
+		e.preventDefault()
+		closePopover();
+		var popover = $($(this).data('popover'));
+		popover.toggleClass('open')
+		e.stopImmediatePropagation();
+	}
 
-  function openPopover(e) {
-    e.preventDefault()
-    closePopover();
-    var popover = $($(this).data('popover'));
-    popover.toggleClass('open')
-    e.stopImmediatePropagation();
-  }
+	function closePopover(e) {
+		if ( $('.popover.open').length > 0 ) {
+			$('.popover').removeClass('open')
+		}
+	}
 
-  function closePopover(e) {
-    if($('.popover.open').length > 0) {
-      $('.popover').removeClass('open')
-    }
-  }
-
-  init();
+	init();
 
 });
 
 
 
-function contact_form(e) {
-       e.preventDefault();
+function contact_form(token) {
+	// e.preventDefault();
+//g-recaptcha-response
 
-       var URL = "https://qjn2mh8df7.execute-api.ap-southeast-2.amazonaws.com/";
-       var DATA = {
-          name: document.getElementById("contact-name").value,
-          email: document.getElementById("contact-email").value,
-          reason: document.getElementById("contact-reason").value,
-          message: document.getElementById("contact-message").value
-        };
+	var URL = "https://qjn2mh8df7.execute-api.ap-southeast-2.amazonaws.com/";
+	var DATA = {
+		name: document.getElementById("contact-name").value,
+		email: document.getElementById("contact-email").value,
+		reason: document.getElementById("contact-reason").value,
+		message: document.getElementById("contact-message").value
+	};
 	var HEADERS = {
 		"Content-Type": "application/json",
 		"Access-Control-Origin": "*"
 	};
 
 	fetch(URL, {
-	    method: "POST",
-	    headers: HEADERS,
-	    body: JSON.stringify(DATA)
+		method: "POST",
+		headers: HEADERS,
+		body: JSON.stringify(DATA)
 	})
 	.then(function(response) {
-	    console.log(response);
+		console.log(response);
 	})
 	.then(function(data) {
-	    // i think this is a failure
-	    console.log(data)
+		// i think this is a failure
+		console.log(data)
 	});
 }
