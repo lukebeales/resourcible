29291b99b2f46214eba6ae99073ba9a833159ae0
Luke Beales <lukebeales@gmail.com>
2022-03-11 01:12:25 +0000

    more css fixes for the footer

--- js/footer.js
+++ js/footer.js
@@ -4,7 +4,7 @@ function footer_init() {
 	document.getElementById('common-footer').innerHTML = `
 		<div class="row">
 			<div class="one-third column">
-				<h3 class="section-heading"><a href="https://resourcible.com" style="text-decoration: none;">Resourcible</a></h3>
+				<h3 class="section-heading"><a href="https://resourcible.com" style="text-decoration: none; color: inherit;">Resourcible</a></h3>
 				A collective of IT industry specialists dedicated to delivering elegant solutions at scale
 			</div>
 			<div class="one-third column">
