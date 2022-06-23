1ff8e8b0cd2c9a9bb4218766f8180c65bbeef458
Ubuntu <ubuntu@ip-172-31-43-114.ap-southeast-2.compute.internal>
2022-06-23 02:09:23 +0000

    updated the email amazon lambda thing

--- js/contact.js
+++ js/contact.js
@@ -35,7 +35,7 @@ function contact_form_init() {
 function contact_form_send(token) {
 	//e.preventDefault();
 
-	var URL = "https://qjn2mh8df7.execute-api.ap-southeast-2.amazonaws.com/";
+	var URL = "https://3hi84hqchg.execute-api.ap-southeast-2.amazonaws.com";
 	var DATA = {
 		name: document.getElementById("contact-name").value,
 		email: document.getElementById("contact-email").value,
